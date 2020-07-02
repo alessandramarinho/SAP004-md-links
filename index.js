7d4e0e21723239d6316e73f49e8db13c8ff2aaa0
alessandramarinho <allemmarinho@gmail.com>
2020-07-02 20:30:13 -0300

    configuração do jest, eslint, babel e manipulação do regex e função

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-module.exports = () => {
-  // ...
-};
+const fs = require ('fs');
+
+function mdLinks (file) {
+return new Promise ((resolve, rejects) => {
+  fs.readFile(file,'utf8', (err, data) => {
+    if (err) {
+      rejects (err.message);
+    } else {
+      const regexMdlinks = /([^[]+)\](\([^)]*)/gm 
+      // \[(\S.*)\](\([^)]*)
+      const array = data.match(regexMdlinks)
+      const result = array.map((el) => {
+        const cutText = el.split('](');
+        const text = cutText[0]
+        const href = cutText[1];
+        return { text, href, file }
+      });
+      resolve(result)
+    }
+  });
+});
+}
+
+// mdLinks(process.argv[2])
+console.log(process.argv)
