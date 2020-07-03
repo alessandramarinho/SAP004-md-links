5fee57c81b4a40e2c25f9e4b42a37f2945327427
alessandramarinho <allemmarinho@gmail.com>
2020-07-03 17:33:34 -0300

    'chalk pra ficar linjuuu'

--- CLI.js
+++ CLI.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 const mdLinks = require ('./index.js');
+const chalk = require('chalk');
 
 mdLinks(process.argv[2])
 .then((result)=>{
-    result.forEach ((el) => {
-    console.log ('\n', el.href, el.text.substring (0, 50));
+    result.forEach((el) => {
+    console.log ('\n', `${chalk.redBright.bold(el.href)}, ${chalk.blueBright.bold(el.text.substring (0, 50))}`);
 })
 })
 .catch(console.error);
