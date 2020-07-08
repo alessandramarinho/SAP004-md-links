f217c92cbc4bd58cd3154854fef84f9774d8a251
alessandramarinho <allemmarinho@gmail.com>
2020-07-08 14:27:08 -0300

    atualizando o retorno do obj do mdLinks

--- index.js
+++ index.js
@@ -1,40 +1,63 @@
-const fs = require ('fs');
+const fs = require('fs');
+const fetch = require('node-fetch')
+const validUrl = require('valid-url')
 
 const mdLinks = (file, option) => {
   const obj = [];
   let arrayLinks = [];
-  return new Promise ((resolve, rejects) => {
-    fs.readFile(file, 'utf8', (err,data) => {
+  return new Promise((resolve, rejects) => {
+    fs.readFile(file, 'utf8', (err, data) => {
       if (err) {
-        rejects (err);
-      } else {
+        rejects(err);
+      }
+      else {
         arrayLinks = data.match(/\[(.*?)\]\((.*?)\)/g);
-        if (option === '--validate') {
+        if (option.includes('--validate')) {
           arrayLinks.forEach(link => {
             const text = link.match(/(?<=\[).+?(?=\])/g);
             const href = link.match(/(?<=\().+?(?=\))/g)
-            obj.push({
-              text: `${text}`,
-              href: `${href}`,
-              status: `${url.isUri(href[0]) ? 'URL OK' : 'URL NOT OK'}`
+            arrayLinks.forEach(link => {
+              obj.push({
+                text,
+                href,
+              })
+            })
+            Promise.all(
+              obj.map(el =>{
+                if (validUrl.isUri(el.href)) {
+                  return fetch(el.href, { method: 'GET' })
+                } else {
+                  return { status: '400' }
+                }
+              })
+            ).then(res => {
+              res.forEach((res, el) => {
+                obj[el].status = res.status
+              })
+              resolve(obj)
             })
           })
-        } else {
-          arrayLinks.forEach(link => {
-            obj.push({
-              text: `${link.match(/(?<=\[).+?(?=\])/g)}`,
-              href: `${link.match(/(?<=\().+?(?=\))/g)}`
+          }else {
+            arrayLinks.forEach(link => {
+              const text = link.match(/(?<=\[).+?(?=\])/g);
+              const href = link.match(/(?<=\().+?(?=\))/g)
+              obj.push({
+                text,
+                href
+              })
             })
-          })
-        }
-        resolve(obj)
-      }
+            console.log(obj)
+          resolve(obj)
+          }
+
+    }})
     })
-  })
-}
+  }
 
-  
 
-      module.exports = mdLinks
+module.exports = mdLinks
+mdLinks(process.argv[2], process.argv)
+// .then (res => console.log (res))
+
 
 
