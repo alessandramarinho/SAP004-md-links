53f4d7500e5d1879774bf9adf7048ecfc524902a
alessandramarinho <allemmarinho@gmail.com>
2020-07-07 22:24:16 -0300

    validate ok

--- CLI.js
+++ CLI.js
@@ -1,16 +1,30 @@
 #!/usr/bin/env node
 
-const mdLinks = require ('./index.js');
-const chalk = require('chalk');
-const validateLinks = require ('./index.js')
+const mdLinks = require('./index.js');
+// const chalk = require('chalk');
 
+mdLinks(process.argv[2], process.argv[3])
+  .then((obj) => {
+    obj.forEach((el) => {
+      if (process.argv[3] === '--validate') {
+        let option = { validate: true }
+        console.log(el.text, el.href, el.status)
+      } else {
+        console.log(el.text, el.href)
+      }
+    })
+  })
 
-mdLinks(process.argv[2])
-.then((result)=>{
-    result.forEach((el) => {
-    console.log ('\n', `${chalk.redBright.bold(el.href)}, ${chalk.blueBright.bold(el.text.substring (0, 50))}`);
-})
-})
-.catch(console.error);
+// .catch((err) => {
+//     (console.log(err.message))
+// }
 
+// console.log(process.argv[2])
 
+// .then((result)=>{
+//     result.forEach((el) => {
+//     console.log ('\n', `${chalk.redBright.bold(el.href)}, ${chalk.blueBright.bold(el.text.substring (0, 50))}`);
+// })
+// })
+// .catch(console.error);
+// console.log (process.argv[3])
--- index.js
+++ index.js
@@ -1,52 +1,40 @@
 const fs = require ('fs');
-const url = require ('valid-url')
 
 const mdLinks = (file, option) => {
-  let obj = [];
+  const obj = [];
   let arrayLinks = [];
   return new Promise ((resolve, rejects) => {
-    fs.readFile(file,'utf8', (err, data) => {
+    fs.readFile(file, 'utf8', (err,data) => {
       if (err) {
         rejects (err);
       } else {
         arrayLinks = data.match(/\[(.*?)\]\((.*?)\)/g);
-        if (option) {
-          // urlExists((link.match(/(?<=\().+?(?=\))/g))[0], (err, res) => { return res })
-          arrayLinks.map(link => {
-            let lin = (link.match (/(?<=\().+?(?=\))/g)) [0]
-
-            obj.push({
-              text: `${link.match(/(?<=\[).+?(?=\])/g)}`, 
-              href: `${link.match(/(?<=\().+?(?=\))/g)}`,
-              status: `${url.isUri(lin) ? 'URL OK' : 'URL NOT OK'}`
-            })
-          // if (validate) {
-          //   urlExists(link.match(/(?<=\().+?(?=\))/g)[0], (err, res) => {
-          //     obj[0].status:res
-          //   })
-          // }
-        })} else {
+        if (option === '--validate') {
           arrayLinks.forEach(link => {
-            obj.push({ 
-              text: `${link.match(/(?<=\[).+?(?=\])/g)}`, 
+            const text = link.match(/(?<=\[).+?(?=\])/g);
+            const href = link.match(/(?<=\().+?(?=\))/g)
+            obj.push({
+              text: `${text}`,
+              href: `${href}`,
+              status: `${url.isUri(href[0]) ? 'URL OK' : 'URL NOT OK'}`
+            })
+          })
+        } else {
+          arrayLinks.forEach(link => {
+            obj.push({
+              text: `${link.match(/(?<=\[).+?(?=\])/g)}`,
               href: `${link.match(/(?<=\().+?(?=\))/g)}`
             })
           })
         }
-        resolve (obj)
-
-
-        // arrayLinks.forEach(link => {
-        //   obj.push({text: `${link.match(/(?<=\[).+?(?=\])/g)}`,
-        //   href: `${link.match(/(?<=\().+?(?=\))/g)}`, 
-        //   file})
-        // })
         resolve(obj)
-      }})})}
+      }
+    })
+  })
+}
 
-      // const validateLink = (obj) => {
-      //   console.log(obj)
-      // }
-      
-      module.exports = mdlinks
-      //module.exports = validateLink
+  
+
+      module.exports = mdLinks
+
+
