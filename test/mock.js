da719ef322af254d710b222a70adcf7c679989ee
alessandramarinho <allemmarinho@gmail.com>
2020-07-12 14:39:12 -0300

    indentação mock

--- test/mock.js
+++ test/mock.js
@@ -1,24 +1,24 @@
 const resultValidate = [
   {
-    text: '8. Guias, dicas e leituras complementares',
-    href: '#8-guias-dicas-e-leituras-complementares',
-    status: '400',
-    file: './test//mock.md',
-    statusText: 'FAIL'
+    "text": '8. Guias, dicas e leituras complementares',
+    "href": '#8-guias-dicas-e-leituras-complementares',
+    "status": '400',
+    "file": './test//mock.md',
+    "statusText": 'FAIL'
   },
   {
-    text: 'Markdown',
-    href: 'https://pt.wikipedia.org/wiki/Markdown',
-    status: 200,
-    file: './test//mock.md',
-    statusText: 'OK'
+    "text": 'Markdown',
+    "href": 'https://pt.wikipedia.org/wiki/Markdown',
+    "status": 200,
+    "file": './test//mock.md',
+    "statusText": 'OK'
   },
   {
-    text: 'Jest',
-    href: 'https://jestjs.io/',
-    status: 200,
-    file: './test//mock.md',
-    statusText: 'OK'
+    "text": 'Jest',
+    "href": 'https://jestjs.io/',
+    "status": 200,
+    "file": './test//mock.md',
+    "statusText": 'OK'
   },
   {
     "file": "./test//mock.md",
