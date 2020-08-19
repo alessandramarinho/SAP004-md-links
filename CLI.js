44a8a2713dc5eb1324557521b69e85c8d4f516fa
alessandramarinho <allemmarinho@gmail.com>
2020-08-19 18:50:47 -0300

    refatoração

--- CLI.js
+++ CLI.js
@@ -1,30 +1,27 @@
 #!/usr/bin/env node
 
-const mdLinks = require('./main.js');
+const mdLinks = require('./src/lib/main');
 const chalk = require('chalk');
-const args = process.argv.slice(2)
 
 mdLinks(process.argv[2], process.argv[3])
   .then((obj) => {
-      if (process.argv[3] === '--validate'){
-        obj.forEach (i =>{
-          console.log(`${chalk.redBright.bold(i.file)}, ${chalk.redBright.bold('text:')}, ${chalk.blueBright.bold(i.text)},  ${chalk.cyan.bold('href:')},  ${chalk.yellow.bold(i.href)},  ${chalk.magenta.bold('status:')}, ${chalk.green.bold((i.status), (i.statusText).toString())}`)
-          })
-      }else if (process.argv.includes('--stats')){
+    if (process.argv[3] === '--validate') {
+      obj.forEach(i => {
+        console.log(`${chalk.redBright.bold(i.file)}, ${chalk.redBright.bold('text:')}, ${chalk.blueBright.bold(i.text)},  ${chalk.cyan.bold('href:')},  ${chalk.yellow.bold(i.href)},  ${chalk.magenta.bold('status:')}, ${chalk.green.bold((i.status), (i.statusText).toString())}`)
+      })
+    } else if (process.argv.includes('--stats')) {
       const stats = obj.map(el => el.href)
       const uniqueLinks = new Set(stats)
       const brokenLinks = obj.filter(el => el.status >= 400 && el.status < 500)
-        console.log(chalk.yellow.bold(`Total Links: ${stats.length}`));
-        console.log(chalk.green.bold(`Total Links Working ${uniqueLinks.size}`));
-        console.log(chalk.red.bold(`Total Broken links: ${brokenLinks.length}`));
-      }else if (args[1]) {
-        console.log(`${chalk.redBright.bold(i.file)}, ${chalk.redBright.bold("Command not found. Did you mean '--validate'?")}`)
-      } else {
-        obj.forEach(i => {
-          console.log(`${chalk.redBright.bold(i.file)}, ${chalk.blueBright.bold('text:', i.text)}, ${chalk.yellow.bold('href:', i.href)}`)
-        })
-      }
-    })
-    .catch(err => console.log(err))
+      console.log(chalk.yellow.bold(`Total Links: ${stats.length}`));
+      console.log(chalk.green.bold(`Total Links Working ${uniqueLinks.size}`));
+      console.log(chalk.red.bold(`Total Broken links: ${brokenLinks.length}`));
+    } else {
+      obj.forEach(i => {
+        console.log(`${chalk.redBright.bold(i.file)}, ${chalk.blueBright.bold('text:', i.text)}, ${chalk.yellow.bold('href:', i.href)}`)
+      })
+    }
+  })
+  .catch(err => console.log(err))
 
 
