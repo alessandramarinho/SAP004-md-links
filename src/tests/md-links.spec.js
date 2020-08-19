aa27e04638d08f7e7c3b21efc7b9f4b402a479cf
alessandramarinho <allemmarinho@gmail.com>
2020-08-19 20:00:26 -0300

    git

--- src/tests/md-links.spec.js
+++ src/tests/md-links.spec.js
@@ -1,4 +1,4 @@
-const mdLinks = require('../index.js');
+const mdLinks = require('../lib/index');
 const mock = require('./mock.js');
 
 
@@ -7,40 +7,29 @@ describe('mdLinks', () => {
     expect(typeof mdLinks).toBe('function')
   })
 
-  it('deve retornar a validação', done => {
-    mdLinks('./test', '--validate').catch (link => {
-      console.log(link)
+  it('deve retornar o diretorio', done => {
+    mdLinks('./exemple').catch (link => {
       expect(link).toBe('Sorry, the file/directory could not be read');
       done()
     });
   });
   it('retorna links com validação', done => {
-    mdLinks('./test//mock.md', '--validate').then(link => {
+    mdLinks('./tests//mock.md', '--validate').then(link => {
       expect(link).toEqual(mock.resultValidate);
       done()
     });
   });
   it('deve retornar um array de objetos', done => {
-    mdLinks('./test//mock.md').then((link) => {
+    mdLinks('./tests//mock.md', '--validate').then((link) => {
       expect(link).toEqual(mock.resultValidate);
       done()
     });
   });
   it('erro', () => {
-    mdLinks('./test/mock.md')
+    mdLinks('./tests/mock.md')
       .catch((err) => {
         expect(err).toBe('Sorry, but there is no archive with that name in this directory');
     })
   })
 })
-
-
-// it('retorna 3 links com status', done => {
-  //   mdLinks('./test/mock.md', '--validate').then(link => {
-  //     console.log(link)
-  //     expect(link).toEqual(resultValidate)
-  //     done()
-  //   })
-  // })
-
 
--- src/tests/mock.js
+++ src/tests/mock.js
@@ -3,32 +3,32 @@ const resultValidate = [
     "text": '8. Guias, dicas e leituras complementares',
     "href": '#8-guias-dicas-e-leituras-complementares',
     "status": '400',
-    "file": './test//mock.md',
+    "file": './tests/mock.md',
     "statusText": 'FAIL'
   },
   {
     "text": 'Markdown',
     "href": 'https://pt.wikipedia.org/wiki/Markdown',
     "status": 200,
-    "file": './test//mock.md',
+    "file": './tests/mock.md',
     "statusText": 'OK'
   },
   {
     "text": 'Jest',
     "href": 'https://jestjs.io/',
     "status": 200,
-    "file": './test//mock.md',
+    "file": './tests/mock.md',
     "statusText": 'OK'
   },
   {
-    "file": "./test//mock.md",
+    "file": "./tests/mock.md",
     "href": "https://github.com/",
     "status": 200,
     "statusText": "OK",
     "text": "GitHub",
     },
     {
-    "file": "./test//mock.md",
+    "file": "./tests/mock.md",
     "href": "https://www.google.com/",
     "status": 200,
     "statusText": "OK",
